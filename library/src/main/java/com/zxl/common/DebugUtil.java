54d0888552cc04f9aea7d6a60f01098900f54258
zxlworking <szzhangxiaolong@163.com>
2018-05-21 10:14:35 +0800

    modify debug control

--- library/src/main/java/com/zxl/common/DebugUtil.java
+++ library/src/main/java/com/zxl/common/DebugUtil.java
@@ -7,11 +7,19 @@ import android.util.Log;
  */
 
 public class DebugUtil {
-    public static final boolean IS_DEBUG = false;
+    public static boolean STATE_OPEN = true;
+    public static boolean STATE_CLOSE = false;
+    public static boolean IS_DEBUG = false;
 
     public static void d(String tag,String message){
         if(IS_DEBUG){
             Log.d(tag,message);
+        }
+    }
+
+    public static void d(Object object,String message){
+        if(IS_DEBUG){
+            Log.d(object.getClass().getSimpleName(),message);
         }
     }
 }
